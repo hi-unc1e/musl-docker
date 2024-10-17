137a85206b6d8408b4e642fa1874f8ee07301d7b
hi-unc1e <67778054+hi-unc1e@users.noreply.github.com>
2024-10-17 02:15:42 +0000

    compile rdp fail

--- build_hydra.sh
+++ build_hydra.sh
@@ -5,7 +5,11 @@ git clone https://github.com/vanhauser-thc/thc-hydra
 cd thc-hydra && git checkout v9.5
 echo "remove /usr/include in `./configure`#163 " && sed -i '163s|/usr/include||' ./configure
 
+# add RDP module deps
+apt-get install -y libfreerdp2-2 freerdp2-dev
 make clean
-./configure --disable-xhydra CC="/usr/bin/musl-gcc -static --static" C_INCLUDE_PATH=/usr/include/x86_64-linux-musl/
+export C_INCLUDE_PATH=/usr/include/x86_64-linux-musl/:/usr/include/x86_64-linux-gnu/openssl/:/musl/include/openssl/:/volume/freerdp-3.8.0/winpr/include:/usr/include/libssh/:/usr/lib/x86_64-linux-gnu/:/usr/include/
+export  CC="/usr/bin/musl-gcc -static --static" 
+./configure --disable-xhydra CC="/usr/bin/musl-gcc -static --static" 
 make -j4
 
